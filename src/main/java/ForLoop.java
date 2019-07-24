c21718ac655c22a365f38c139ac6eadd632d1f9a
egambarampanneerselvam <egambaram.panneerselvam@directlinegroup.co.uk>
2019-07-24 22:18:25 +0100

    Ega: Added ForLoop class

---
+++ src/main/java/ForLoop.java
@@ -0,0 +1,16 @@
+public class ForLoop {
+
+    public static void main(String [] ags){
+
+        int i;
+
+        for (i=1; i<=25; i++){
+
+            System.out.println(i);
+
+        }
+        System.out.println("Loop successfully executed for "+(i-1)+" times");
+
+    }
+
+}
