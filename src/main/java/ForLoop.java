45a841c99acdaa1716c48d64020db2f3851ebfb3
egambarampanneerselvam <egambaram.panneerselvam@directlinegroup.co.uk>
2019-07-24 22:22:16 +0100

    Ega: Followed alignment (command + Option + L in mac)

--- src/main/java/ForLoop.java
+++ src/main/java/ForLoop.java
@@ -1,15 +1,15 @@
 public class ForLoop {
 
-    public static void main(String [] ags){
+    public static void main(String[] ags) {
 
         int i;
 
-        for (i=1; i<=25; i++){
+        for (i = 1; i <= 25; i++) {
 
             System.out.println(i);
 
         }
-        System.out.println("Loop successfully executed for "+(i-1)+" times");
+        System.out.println("Loop successfully executed for " + (i - 1) + " times");
 
     }
 
