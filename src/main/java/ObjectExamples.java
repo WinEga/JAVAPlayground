dad81e72dd58dad3e28c71242af1ab4a2dc8d393
Priya <priyadharshini.raghu@directlinegroup.co.uk>
2019-08-02 16:42:07 +0530

    Class2 2

---
+++ src/main/java/ObjectExamples.java
@@ -0,0 +1,9 @@
+public class ObjectExamples {
+    static int Employid;
+    int a1,a2,a3;
+
+
+}
+
+
+
---
+++ src/main/java/Person.java
@@ -0,0 +1,19 @@
+public class Person
+{
+    String name;
+    char gender;
+    Person()
+    {
+        System.out.println("Details of the person");
+    }
+    Person(String n,char g)
+    {
+        name = n;
+        gender =g;
+    }
+    void display()
+    {
+        System.out.println("Name :"+ name);
+        System.out.println("Gender :"+gender);
+    }
+}
