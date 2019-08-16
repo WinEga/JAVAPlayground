c8c3e67f72aa30dbc3e3bcd47b1d4aacce762dad
egambarampanneerselvam <egambaram.panneerselvam@directlinegroup.co.uk>
2019-08-16 12:01:44 +0100

    Ega: Override method

---
+++ src/main/java/OverrideMethod.java
@@ -0,0 +1,27 @@
+//Sub-class overrides the super class
+
+class class1{
+    public void show(){
+
+        System.out.println("In class1");
+    }
+
+}
+
+class class2 extends class1 {
+@Override
+    public void show(){
+        System.out.println("In class2");
+    }
+
+}
+
+
+public class OverrideMethod {
+
+    public static void main(String[] args) {
+        class2 obj1 = new class2();
+        obj1.show();
+
+    }
+}
