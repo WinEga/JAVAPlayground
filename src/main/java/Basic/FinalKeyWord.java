33fbb8a5676e8cb4b2867a40ff6662695596b809
egambarampanneerselvam <egambaram.panneerselvam@directlinegroup.co.uk>
2019-09-07 11:14:39 +0100

    Ega: Final keyword

---
+++ src/main/java/Basic/FinalKeyWord.java
@@ -0,0 +1,27 @@
+/*
+
+We can use final keyword with
+variable --> it will become Constant
+class --> then not possible to extend your class
+method --> not possible to override your method
+
+*/
+
+package Basic;
+
+class Fan {
+    final int J = 4;
+    public void show(){
+        System.out.println("Inside the show method");
+    }
+    //J=40;
+}
+
+public class FinalKeyWord {
+    public static void main(String[] args) {
+        Fan obj1 = new Fan();
+        System.out.println(obj1.J);
+        obj1.show();
+        
+    }
+}
