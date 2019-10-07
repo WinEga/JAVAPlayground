e268cc34dacb1ad5931f507d014b999cf8d6bae0
egambarampanneerselvam <egambaram.panneerselvam@directlinegroup.co.uk>
2019-10-07 21:07:03 +0100

    Ega: Annonymous class replayed by Lambda expression

--- src/main/java/OOPS/LambdaExpression.java
+++ src/main/java/OOPS/LambdaExpression.java
@@ -1,13 +1,22 @@
 package OOPS;
 
-interface Integration{
+interface Integration {
     void contact();
 }
+
 public class LambdaExpression {
     public static void main(String[] args) {
 
-                Integration obj1 = () ->
-                        System.out.println("I am the contact from Insurance policy");
-                obj1.contact();
+//                Integration obj1 = new Integration() {
+//                    public void contact() {
+//                        System.out.println("I am the contact from Insurance policy");
+//
+//                    }
+//                };
+//        obj1.contact();
+
+        Integration obj1 = () -> System.out.println("I am the contact from Insurance policy");
+
+        obj1.contact();
     }
 }
