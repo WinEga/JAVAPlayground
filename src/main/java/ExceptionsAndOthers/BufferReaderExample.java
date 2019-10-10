29e86cf5e1878dc374b50fcfaa0cfd362252ace6
egambarampanneerselvam <egambaram.panneerselvam@directlinegroup.co.uk>
2019-10-10 22:15:42 +0100

    Ega: try catch example with few if conditions in BufferedReader

---
+++ src/main/java/ExceptionsAndOthers/BufferReaderExample.java
@@ -0,0 +1,32 @@
+package ExceptionsAndOthers;
+
+import com.sun.xml.internal.ws.api.streaming.XMLStreamReaderFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BufferReaderExample {
+    public static void main(String[] args) {
+        double n = 0;
+        System.out.println("Enter your SSLC mark : ");
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            n = Double.parseDouble(br.readLine());
+            if (n < 0) {
+                System.out.println("Oh negative! Please enter the valid mark");
+
+            } else if (n > 500) {
+                System.out.println("Oh my dear, you are more than the total mark 500! Please enter the valid mark");
+
+            } else {
+                System.out.println("Your percentage is " + (n / 500) * 100 + "%");
+            }
+
+
+        } catch (Exception e) {
+            System.out.println("Requested to enter your mark");
+
+        }
+    }
+}
