c81d0318b4fdf81587027240a61df8764f83fbba
Ram <ramkumar.s@directlinegroup.co.uk>
2019-10-15 11:25:36 +0100

    Ram- updated

--- src/main/java/OOPS/LambdaExpression2.java
+++ src/main/java/OOPS/LambdaExpression2.java
@@ -21,7 +21,7 @@ public class LambdaExpression2 {
         // Team members name
         ArrayList<String> List = new ArrayList<>();
         List.add("Ega");
-        List.add("Ram");
+        List.add("Ramkumar");
         List.add("Naveen");
         List.add("Priya");
         List.add("Sabitha");
